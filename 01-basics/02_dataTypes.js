b1d9d63e905230e718afb55de6ad0d25c1233590
ADITYA GUPTA <ag7023587@gmail.com>
2024-08-21 17:56:38 +0530

    datatypes

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict" // treat all JS code as newer version
+
+// datatypes
+let age =20 // number 
+console.log(typeof age);
+
+let name = 'John Doe' // string
+console.log(typeof name);
+
+let isStudent = true // boolean
+console.log(typeof isStudent);
+
+let city = null // null
+console.log(typeof city); // this should be object(imp)
+
