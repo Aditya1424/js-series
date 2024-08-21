1e1c60762a36d7d4a1ab9f295c8f7db076d6a74a
ADITYA GUPTA <ag7023587@gmail.com>
2024-08-21 19:04:41 +0530

    comparsions in datatypes

---
+++ 01-basics/04_comparsions.js
@@ -0,0 +1,24 @@
+console.log('2'>1) // true
+
+// imp ones
+console.log(null>0) // false
+console.log(null==0);// false
+console.log(null<0); // false
+console.log(null>=0); // true
+
+// in case of null, equality check and comparsion check works differently
+// while comparsions null is converted to 0 and then compares
+// but not in equality check
+
+// == vs ===
+// == operator only checks the value while === or strictly check operator not only checks the 
+// values but also checks the data type 
+console.log('2'==2); // true
+console.log('2'===2); // false
+
+console.log(true==1); // true
+console.log(true===1); // false
+
+
+
+
